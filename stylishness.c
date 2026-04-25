2b46517709aced9d00fa13d03d08c7bb6b8b9709
tejashree-botta <bottatejashree@gmail.com>
2026-04-25 11:45:51 +0530

    Add files via upload

---
+++ stylishness.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main ()
+{
+    int you,date;
+    printf("enter the stylishness of you and date \n");
+    scanf("%d%d",&you,&date);
+    if(you<=2||date<=2)
+    {
+        printf("no=0");
+    }
+    else if(you>=8||date>=8)
+    {
+        printf("yes=2");
+    }
+    else
+    {
+        printf("maybe=1");
+    }
+}
